a0ad4211b2c12c188299a2cb7e707d3cd9ab31fd
aryaadwii123 <109120502+aryaadwii123@users.noreply.github.com>
2022-08-23 10:46:52 +0700

    tugasfungsi

---
+++ MuhammadHafiyyDanAryaDwi_TugasFungsi.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Muhammad hafiyy arreza & Arya Dwi
+ * XII RPL 1
+ * No:18 & 3
+ */
+echo " <br>";
+function volume balok ($panjang,$lebar,$tinggi) {
+    return ($panjang*$lebar*$tinggi);
+}
+echo " 1.Diketahui P =15, L =12, T =8, Tentukan volume balok tersebut =".volumebalok (15,12,8);
+echo " <br>";
+function volumekerucut ($jari2,$tinggi) {
+    return (1/3*M_1_PI*$jari2*$jari2*$tinggi);
+}
+echo " 2.Diketahui r =14, t =15. Tentukan volume kerucut tersebut =".volumekerucut (14,15);
+
+?>
